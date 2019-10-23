45c4efc42b2d1a8ae5acc9b41b79565e5a2a80a2
WANhuimin <1975871730@qq.com>
2019-10-23 22:29:18 +0800

    提交代码

---
+++ Practice_9_6.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+import java.util.Date;
+import java.util.Random;
+public class Practice_9_6 {
+	public static void main(String[] args) {
+		final int N= 100000;
+		int[] number = new int[N];
+		Random random = new Random();
+		for (int i = 0; i < N; i++){
+			number[i] = random.nextInt(100000);
+                }
+		StopWatch stopWatch = new StopWatch();
+		sort(number, 0, N - 1);
+		stopWatch.stop();
+		System.out.println(stopWatch.getElapsedTime() + "ms");
+		
+	}
+	
+	//将数组第一个数作为中间数，放到数组中正确的位置，并返回其位置
+	public static int mid(int[] a,int low,int high) {
+		int middle = a[low];
+		while (low < high) {
+			for (; low < high; high--) {
+				if (middle > a[high]) {
+					a[low] = a[high];
+					low++;
+					break;
+				}
+			}
+			for (; low < high; low++) {
+				if (a[low] > middle) {
+					a[high] = a[low];
+					high--;
+					break;
+				}
+			} 
+		}
+		a[low] = middle;
+		return low;
+	}
+	//将数组分成两份。递归。
+	public static void sort(int[] a, int low, int high) {
+		if (low >= high)
+			return;
+		int middle;
+		middle = mid(a, low, high);	
+		sort(a, low, middle - 1);
+		sort(a, middle + 1, high);	
+	}
+
+}
+ class StopWatch {
+	private Date startTime;
+	private Date endTime;
+	public StopWatch() {
+		startTime = new Date();
+	}
+	public void start() {
+		startTime = new Date();
+	}	
+	public void stop() {
+		endTime = new Date();
+	}
+	public long getElapsedTime() {
+		return endTime.getTime() - startTime.getTime();
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+}
